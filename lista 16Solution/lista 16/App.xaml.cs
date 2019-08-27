5f01376c645737f3e29dbf36896fe63a5fd1ea20
Leonardo da Silva Monteiro <leonardodasilva.ifrn@gmail.com>
2019-08-27 16:21:09 -0300

    lista 16

---
+++ lista 16Solution/lista 16/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace lista_16
+{
+	/// <summary>
+	/// Interação lógica para App.xaml
+	/// </summary>
+	public partial class App : Application
+	{
+	}
+}
